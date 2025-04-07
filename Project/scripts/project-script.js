8afbfaf5f5e8db91d16213aeb33011ebd130ac67
Zeke Houser <zekecs.111@gmail.com>
2025-04-06 23:18:52 -0400

    client project skeleton

---
+++ Project/scripts/project-script.js
@@ -0,0 +1,2 @@
+const createAccountBtn = document.getElementById('create-account-btn');
+createAccountBtn.addEventListener('click', function() {window.location.href = 'create_account.html'});
---
+++ Project/scripts/redirect-to-login.js
@@ -0,0 +1,2 @@
+const createAccount = document.getElementById('createAccount');
+createAccount.addEventListener('click', function(event) {event.preventDefault(); window.location.href = 'login.html'});
